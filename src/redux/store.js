a4d63426c79a000854e9155f8c1916e166bd0539
Alexander Nikiforov <keristcor@gmail.com>
2021-05-12 16:23:53 +0300

    added redux-persist to save cart items; changed icon

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,9 +1,19 @@
 import {createStore, applyMiddleware} from 'redux';
 import logger from 'redux-logger';
 import createSagaMiddleware from 'redux-saga';
+import { persistStore, persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
 
 import rootReducer from './root-reducer';
 import { rootSagas } from './root-sagas';
+
+const persistConfig = {
+    key: 'root',
+    storage,
+    whitelist: ['cart']
+}
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 const sagaMiddleware = createSagaMiddleware();
 const middlewares = [sagaMiddleware];
@@ -12,6 +22,8 @@ if(process.env.NODE_ENV === 'development') {
     middlewares.push(logger);
 }
 
-export const store = createStore(rootReducer, applyMiddleware(...middlewares))
+export const store = createStore(persistedReducer, applyMiddleware(...middlewares));
 
 sagaMiddleware.run(rootSagas);
+
+export const persistor = persistStore(store);
